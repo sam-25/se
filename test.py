2d6a6b0dd8b732dfc0dd2de972c1a7997170e133
sam-25 <73120494+sam-25@users.noreply.github.com>
2022-11-25 09:47:02 +0530

    Add files via upload

---
+++ test.py
@@ -0,0 +1,25 @@
+import unittest
+from program import factorial
+
+class TestFactorial(unittest.TestCase):
+	def test_Factorial1(self):
+		result1=factorial(1)
+		self.assertEquals(result1,1)
+	def test_Factorial2(self):
+		result2=factorial(2)
+		self.assertEquals(result2,2)
+	def test_Factorial3(self):
+		result2=factorial(3)
+		self.assertEquals(result2,6)
+	def test_Factorial4(self):
+		result4=factorial(4)
+		self.assertEquals(result2,24)
+	def test_Factorial5(self):
+		result5=factorial(5)
+		self.assertEquals(result2,120)
+	def test_Factorial6(self):
+		result6=factorial(6)
+		self.assertEquals(result2,720)
+
+if __name__ == '__main__':
+	unittest.main()
