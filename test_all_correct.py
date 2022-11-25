eefb188bc71fda3df49e4b74ae96602c582f0bc9
sam-25 <73120494+sam-25@users.noreply.github.com>
2022-11-25 10:03:16 +0530

    Update test_all_correct.py

--- test_all_correct.py
+++ test_all_correct.py
@@ -9,17 +9,17 @@ class TestFactorial(unittest.TestCase):
 		result2=factorial(2)
 		self.assertEquals(result2,2)
 	def test_Factorial3(self):
-		result2=factorial(3)
-		self.assertEquals(result2,6)
+		result3=factorial(3)
+		self.assertEquals(result3,6)
 	def test_Factorial4(self):
 		result4=factorial(4)
-		self.assertEquals(result2,24)
+		self.assertEquals(result4,24)
 	def test_Factorial5(self):
 		result5=factorial(5)
 		self.assertEquals(result5,120)
 	def test_Factorial6(self):
 		result6=factorial(6)
-		self.assertEquals(result2,720)
+		self.assertEquals(result6,720)
 
 if __name__ == '__main__':
 	unittest.main()
